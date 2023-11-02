0fbc332f3c23d127cb3d1670db9c96a6904e0364
deepak1051 <lightyagamigoku@gmail.com>
2023-11-02 20:59:51 +0530

    final

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+import { useState } from 'react';
+import _questions from './data';
+
+const shuffle = (array) => {
+  return array.sort(() => Math.random() - 0.5);
+};
+
+export default function App() {
+  const questions = shuffle(_questions);
+
+  const [selectedOptionIndex, setSelectedOptionIndex] = useState(-1);
+  const [selectedOption, setSelectedOption] = useState(-1);
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+
+  const handleAnswerOptionClick = (option, i) => {
+    setSelectedOptionIndex(i);
+    setSelectedOption(option);
+  };
+
+  const handleAnswerButtonClick = () => {
+    setSelectedOptionIndex(-1);
+    if (selectedOption === questions[currentQuestion].answer) {
+      setScore((prev) => prev + 1);
+    }
+
+    if (currentQuestion === questions.length - 1) {
+      setShowScore(true);
+    } else {
+      setCurrentQuestion((prev) => prev + 1);
+    }
+  };
+
+  const resetQuiz = () => {
+    setShowScore(false);
+    setCurrentQuestion(0);
+    setScore(0);
+    setSelectedOptionIndex(-1);
+  };
+
+  return (
+    <div className="app">
+      {showScore ? (
+        <div className="score-section">
+          You scored {score} out of {questions.length}
+          <button onClick={resetQuiz}>Reset Quiz</button>
+        </div>
+      ) : (
+        <>
+          <div className="question-section">
+            <div className="question-count">
+              <span>Question {currentQuestion + 1}</span>/{questions.length}
+            </div>
+            <div className="question-text">
+              {questions[currentQuestion].question}
+            </div>
+          </div>
+          <ul className="answer-section">
+            {questions[currentQuestion].options.map((option, i) => (
+              <li
+                onClick={() => handleAnswerOptionClick(option, i)}
+                key={option}
+                className={i === selectedOptionIndex ? 'active' : ''}
+              >
+                {option}
+              </li>
+            ))}
+            <button onClick={handleAnswerButtonClick}>Submit</button>
+          </ul>
+        </>
+      )}
+    </div>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,33 @@
+export default [
+  {
+    question: 'What is the capital of France?',
+    options: ['New York', 'London', 'Paris', 'Dublin'],
+    answer: 'Paris',
+  },
+  {
+    question: 'Who is CEO of Tesla?',
+    options: ['Jeff Bezos', 'Elon Musk', 'Bill Gates', 'Tony Stark'],
+    answer: 'Elon Musk',
+  },
+  {
+    question: 'The iPhone was created by which company?',
+    options: ['Apple', 'Intel', 'Amazon', 'Microsoft'],
+    answer: 'Apple',
+  },
+  {
+    question: 'How many Harry Potter books are there?',
+    options: ['1', '4', '6', '7'],
+    answer: '7',
+  },
+
+  {
+    question: 'Who painted the Mona Lisa?',
+    options: [
+      'Vincent Van Gogh',
+      'Pablo Picasso',
+      'Leonardo Da Vinci',
+      'Claude Monet',
+    ],
+    answer: 'Leonardo Da Vinci',
+  },
+];
